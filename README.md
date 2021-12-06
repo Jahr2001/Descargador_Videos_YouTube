f1b0ccdda6b8f6760b232a0b6faeaaf262c572dd
Jahr2001 <193213@ids.upchiapas.edu.mx>
2021-12-06 00:03:16 -0600

    commit final

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Descargador_Videos_YouTube
 Programa para descargar videos de YouTube con Python, aplicando concurrencia.
 
-Getzamany Magdaleno Serrano - 193245,
-
-José Alberto Hernández Ramírez - 193213
+193245 - Getzamany Magdaleno Serrano
+193213 - José Alberto Hernández Ramírez
--- downloader.py
+++ downloader.py
@@ -1,18 +1,18 @@
 from PyQt5.QtWidgets import QMessageBox
 from concurrent.futures import ThreadPoolExecutor
 from pytube import YouTube
+
 #urls = ['https://www.youtube.com/watch?v=T8Z1VT1RE6E','https://www.youtube.com/watch?v=CjmzDHMHxwU','https://www.youtube.com/watch?v=lc5JJTQa4r8', 'https://www.youtube.com/watch?v=aD_kQBi6gOY', 'https://www.youtube.com/watch?v=0DNCCl3sCjY', 'https://www.youtube.com/watch?v=bm-PDiLiHCI','https://www.youtube.com/watch?v=mcWXvFC45hc','https://www.youtube.com/watch?v=oKAnOzIu66c']
+
 urls = []
 data = []
-#urls = ['https://www.youtube.com/watch?v=T8Z1VT1RE6E']
-#directorio_actual = "D:\\Documentos\\7mo Cuatrimestre\\Programación Concurrente\\Tercer Corte\\DescargadorVideosYouTube\\Video"
 
 directorio_actual = ''
 
 def descargando(urls):
     if not urls:
         print('VACIO ARREGLO')
-        #QMessageBox.warning(None, 'Lista Vacia','Lista de videos vacia')
+        
     else:
         video = YouTube(urls)
         print(f'--------------------------------------')
@@ -22,19 +22,30 @@ def descargando(urls):
         print(f'Lenght: {(video.length)/60} Min')
         print(f'URL V: {urls}')
         print(f'--------------------------------------')
-        data.append((video.title, video.author, video.channel_url, video.length, urls))
+        
+        title = video.title
+        author = video.author
+        urlChannel = video.channel_url
+        time = video.length
+        length = str(round((time/60),2)) + ' Min'
+        url = urls
+        
+        data.append((title,author,urlChannel,length,url))
+
         stream = video.streams.get_highest_resolution()
         stream.download(directorio_actual)
         print(f'DESCARGA COMPLETADA!! --> {video.title} URL--> {urls}')
+        
 
 def subProcesosMultiples():
-    print('SubProcesos Multiples!!!!')
+    
     if not urls:
-        print('NO HAY URL')
-        QMessageBox.warning(None, 'Lista Vacia','Lista de videos vacia')
+        QMessageBox.critical(None, 'La lista esta Vacia','Lista de videos a descargar esta vacía!!\nIngrese la url del video que desea descargar!!!')
     else:
-        print('Entra a la descarga ddel video')
+        
         with ThreadPoolExecutor(max_workers=(len(urls))) as excuter:
             excuter.map(descargando, urls)
+
+        QMessageBox.information(None, 'Descarga!!!','La descarga esta completada!!')
 
         urls.clear()
